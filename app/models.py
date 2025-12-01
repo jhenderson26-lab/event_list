910ee74d4f77c66ab55df23c97d4d919c247e86d
jhenderson26-lab <jhenderson26@basecampcodingacademy.org>
2025-12-01 13:30:02 -0600

    orderlist, deletEoldevent

--- app/models.py
+++ app/models.py
@@ -4,14 +4,15 @@ from datetime import date
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    set_date = models.IntegerField()
+    set_date = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     event_date = models.DateField(null=True, blank=True)
 
     @property
     def until_event(self):
         today = date.today()
-        until = self.event_date - today
-        until_stripped = str(until).split("day", 1)[0]
-        num_until = int(until_stripped.strip())
-        return num_until
+        if self.event_date != None:
+            until = self.event_date - today
+            until_stripped = str(until).split("day", 1)[0]
+            num_until = int(until_stripped.strip())
+            return num_until
--- app/views.py
+++ app/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, redirect
 from . import forms
 from . import models
-from datetime import timedelta
+from datetime import timedelta, datetime
+from datetime import date
 
 # Create your views here.
-
+def check_event(day):
+    int_date = int((f'{day.event_date.year}' + f'{day.event_date.month}' + f'{day.event_date.day}'))
+    tday = date.today()
+    int_today = int((f'{tday.year}' + f'{tday.month}' + f'{tday.day}'))
+    if int_date > int_today:
+        day.save()
+    else:
+        day.delete()
+    
 def home(request):
     event = forms.EventForm()
     event_list = models.Event.objects.all()
+    list_order = models.Event.objects.all().order_by("event_date")
     for events in event_list:
-        if events.until_event <= 0:
+        if events.until_event == None:
             events.delete()
-    context = {'event': event, 'text': event_list}
+        check_event(events)
+    context = {'event': event, 'text': list_order}
     return render(request, 'base.html', context)
 
 def event_view(request):
@@ -20,12 +31,12 @@ def event_view(request):
         if form.is_valid():
             form.save()
             for day in models.Event.objects.all():
-                change_date = day.created_at + timedelta(days=(day.set_date -1))
+                change_date = day.created_at + timedelta(days=(day.set_date ))
                 day.event_date = change_date
-                day.save()
-            
+                check_event(day)
         return redirect('home')
 
+ 
 def delete_event(request, event_id):
     event = models.Event.objects.get(id=event_id)
     event.delete()
