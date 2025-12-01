cc047d1ea7f69f2a28efbe58a95089795dabced2
Jason-R-Henderson <jasonryanh2017@icloud.com>
2025-11-30 23:53:17 -0600

    added count down

--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    set_date = models.PositiveSmallIntegerField()
+    set_date = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    event_date = models.DateField(null=True, blank=True)
+
+    @property
+    def until_event(self):
+        today = date.today()
+        until = self.event_date - today
+        until_stripped = str(until).split("day", 1)[0]
+        num_until = int(until_stripped.strip())
+        return num_until
--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from . import forms
 from . import models
+from datetime import timedelta
 
 # Create your views here.
 
 def home(request):
     event = forms.EventForm()
-    context = {'event': event}
+    event_list = models.Event.objects.all()
+    for events in event_list:
+        if events.until_event <= 0:
+            events.delete()
+    context = {'event': event, 'text': event_list}
     return render(request, 'base.html', context)
 
 def event_view(request):
@@ -14,13 +19,14 @@ def event_view(request):
         form = forms.EventForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('event')
-    event_list = models.Event.objects.all()
-    context = {'text': event_list}
-    return render(request, 'other.html', context)
+            for day in models.Event.objects.all():
+                change_date = day.created_at + timedelta(days=(day.set_date -1))
+                day.event_date = change_date
+                day.save()
+            
+        return redirect('home')
 
 def delete_event(request, event_id):
     event = models.Event.objects.get(id=event_id)
     event.delete()
-    return redirect('event')
- 
+    return redirect('home')
